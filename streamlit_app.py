40362200d9e1bd627b9dca2d3cac6f482ff68f17
Raghad Mahaia Alotaibi <Raghad.alotaibi01@gmail.com>
2024-12-23 20:29:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+import pickle
+import numpy as np
+model = pickle.load(open('trained_model.sav', 'rb'))
+st.title('Iris Prediction')
 
-st.title('🎈 App Name')
+sepel_len= st.number_input('sepel lenght')
+spel_w= st.number_input('sepel width')
+patel_len= st.number_input('patel lenght')
+patel_w= st.number_input('patel width')
 
-st.write('Hello world!')
+input_data =[sepel_len,spel_w,patel_len,patel_w]
+
+result = ''
+if st.button('Result'):
+    input_data = np.asarray(input_data).reshape(1,-1)
+    result = model.predict(input_data)[0]
+st.success(result)
